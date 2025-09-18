6008b8e0f93db0ef1c29d48e8e3b74d66f20028c
SoL <sonlu1207@gmail.com>
2025-09-18 17:12:15 +0700

    Add: Day4Lab

---
+++ Day4Lab-Layout/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day4Lab_Layout.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
